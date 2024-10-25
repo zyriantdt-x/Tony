ec3c2e116bdbd946177d671369d45b422b3c95cf
Ellis <me@ellis.pw>
2024-10-25 20:17:56 +0100

    create structure for socket event handlers

---
+++ Tony/Server/SocketEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tony.Server;
+internal class SocketEventHandler : ISocketEventHandler {
+    public void SocketOpen( IWebSocketConnection socket ) {
+
+    }
+
+    public void SocketClose( IWebSocketConnection socket ) {
+
+    }
+
+    public void SocketMessage( IWebSocketConnection socket, string message ) {
+    
+    }
+}
