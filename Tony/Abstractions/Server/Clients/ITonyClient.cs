caf8270423851218f84bdfad97a2e8fba15d1854
Ellis <me@ellis.pw>
2024-10-26 13:53:29 +0100

    build structure for tonyclient

---
+++ Tony/Abstractions/Server/Clients/ITonyClient.cs
@@ -0,0 +1,12 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tony.Server.Clients;
+internal interface ITonyClient {
+    string Uuid { get; }
+    IWebSocketConnection FleckConnection { get; init; }
+}
---
+++ Tony/Server/Clients/TonyClient.cs
@@ -0,0 +1,17 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tony.Server.Clients;
+internal class TonyClient {
+    public string Uuid { get; }
+
+    public required IWebSocketConnection FleckConnection { get; init; }
+
+    public TonyClient() {
+        this.Uuid = Guid.NewGuid().ToString();
+    }
+}
