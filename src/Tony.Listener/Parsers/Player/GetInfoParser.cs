4cb0d52d54d98000b16fe6245537ff40d664d9a8
Ellis <me@ellis.pw>
2025-03-03 21:25:57 +0000

    more boilerplate

---
+++ src/Tony.Listener/Parsers/Player/GetInfoParser.cs
@@ -0,0 +1,8 @@
+﻿using Tony.Listener.Messages.Player;
+using Tony.Listener.Tcp;
+
+namespace Tony.Listener.Parsers.Player;
+[Header( 7 )]
+internal class GetInfoParser : IParser<GetInfoMessage> {
+    public GetInfoMessage Parse( Message message ) => new();
+}
