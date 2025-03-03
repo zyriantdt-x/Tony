adaba8b32d3e7182bc9dc27a113b081fc8012eaa
Ellis <me@ellis.pw>
2025-03-03 21:29:42 +0000

    hopefully done with the boilerplate

--- src/Tony.Listener/Handlers/Player/GetCreditsHandler.cs
+++ src/Tony.Listener/Handlers/Player/GetCreditsHandler.cs
@@ -1,3 +1,11 @@
-﻿namespace Tony.Listener.Handlers.Player;
-internal class GetCreditsHandler {
+﻿using Tony.Listener.Messages.Player;
+using Tony.Listener.Parsers;
+using Tony.Listener.Tcp.Clients;
+
+namespace Tony.Listener.Handlers.Player;
+[Header( 8 )]
+internal class GetCreditsHandler : IHandler<GetCreditsMessage> {
+    public Task Handle( TonyClient client, GetCreditsMessage message ) {
+    
+    }
 }
