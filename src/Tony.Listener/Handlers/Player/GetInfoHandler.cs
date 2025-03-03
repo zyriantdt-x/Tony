c4d3dfa75b792ffbad906be2dcd54586dc4b7317
Ellis <me@ellis.pw>
2025-03-03 21:26:04 +0000

    implement handler

---
+++ src/Tony.Listener/Handlers/Player/GetInfoHandler.cs
@@ -0,0 +1,25 @@
+﻿using Tony.Listener.Composers.Player;
+using Tony.Listener.Messages.Player;
+using Tony.Listener.Parsers;
+using Tony.Listener.Services.Player;
+using Tony.Listener.Tcp.Clients;
+using Tony.Protos;
+
+namespace Tony.Listener.Handlers.Player;
+[Header( 7 )]
+internal class GetInfoHandler : IHandler<GetInfoMessage> {
+    private readonly PlayerDataService player_data;
+
+    public GetInfoHandler( PlayerDataService player_data ) {
+        this.player_data = player_data;
+    }
+
+    public async Task Handle( TonyClient client, GetInfoMessage message ) {
+        if( client.PlayerId is null )
+            return;
+
+        UserObjectResponse uo = await this.player_data.GetUserObject( client.PlayerId ?? throw new Exception() );
+
+        await client.SendAsync( new UserObjectComposer( uo ) );
+    }
+}
