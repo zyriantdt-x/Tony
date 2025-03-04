7fc11e0585f4fd9442a1e05f2372247a9f5fa47a
Ellis <me@ellis.pw>
2025-03-04 23:18:15 +0000

    add more ef

--- src/Tony.Rooms/Storage/RoomStorage.cs
+++ src/Tony.Rooms/Storage/RoomStorage.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Tony.Rooms.Storage.Entities;
 
 namespace Tony.Rooms.Storage;
 
 public class RoomStorage : DbContext {
+    public DbSet<Category> Categories { get; set; }
+
     public RoomStorage( DbContextOptions<RoomStorage> options ) : base( options ) { }
 }
