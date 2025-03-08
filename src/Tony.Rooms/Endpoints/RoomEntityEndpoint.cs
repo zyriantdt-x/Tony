a3d25334916691cbd3f656fb7e0357b04ef4aa52
Ellis <me@ellis.pw>
2025-03-08 15:20:45 -0800

    start adding grpc endpoint

---
+++ src/Tony.Rooms/Endpoints/RoomEntityEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using Tony.Rooms.Services;
+using Tony.Shared.Dto;
+using Tony.Shared.Protos;
+
+namespace Tony.Rooms.Endpoints;
+
+public class RoomEntityEndpoint : Shared.Protos.RoomEntityEndpoint.RoomEntityEndpointBase {
+    private readonly RoomEntityService entity_service;
+
+    public RoomEntityEndpoint( RoomEntityService entity_service ) {
+        this.entity_service = entity_service;
+    }
+    public async override Task<GetEntitiesInRoomResponse> GetEntitiesInRoom( GetEntitiesInRoomRequest request, ServerCallContext context ) {
+        ICollection<RoomEntityDto> entities = await this.entity_service.GetEntitiesInRoom( request.RoomId );
+
+        GetEntitiesInRoomResponse res = new();
+        res.Entities.AddRange( entities.Select( entity => new RoomEntityProto() {
+            InstanceId = entity.InstanceId,
+            EntityId = entity.EntityId,
+            EntityType = ( int )entity.EntityType,
+            Username = entity.Username,
+            Figure = entity.Figure,
+            Sex = entity.Sex,
+            Motto = entity.Motto,
+            Badge = entity.Badge,
+            PosX = entity.PosX,
+            PosY = entity.PosY,
+            PosZ = entity.PosZ
+        } ) );
+
+        return res;
+    }
+
+    public async override Task<AddEntityToRoomResponse> AddEntityToRoom( AddEntityToRoomRequest request, ServerCallContext context ) { 
+    
+    }
+}
