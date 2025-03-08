1f0c416b1ef72e7238ed4775f9111fa09f71732d
Ellis <me@ellis.pw>
2025-03-08 14:53:10 -0800

    add composer

---
+++ src/Tony.Listener/Composers/Room/UserObjectsComposer.cs
@@ -0,0 +1,10 @@
+﻿using Tony.Listener.Tcp;
+
+namespace Tony.Listener.Composers.Room;
+class UserObjectsComposer : ComposerBase {
+    public override short Header => 28;
+
+    public override Message Compose() {
+
+    }
+}
