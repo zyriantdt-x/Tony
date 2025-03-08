1325c3ff7828abcee5e2551b92762934890c027f
Ellis <me@ellis.pw>
2025-03-08 14:57:59 -0800

    add roomentityservice

--- src/Tony.Rooms/Services/RoomEntityService.cs
+++ src/Tony.Rooms/Services/RoomEntityService.cs
@@ -1,4 +1,7 @@
-﻿using Tony.Rooms.Cache;
+﻿using StackExchange.Redis;
+using System.Text.Json;
+using Tony.Rooms.Cache;
+using Tony.Shared.Dto;
 
 namespace Tony.Rooms.Services;
 
@@ -8,4 +11,10 @@ public class RoomEntityService {
     public RoomEntityService( RoomEntityCache cache ) {
         this.cache = cache;
     }
+
+    public Task<ICollection<RoomEntityDto>> GetEntitiesInRoom( int room_id ) => this.cache.GetEntitiesInRoom( room_id );
+
+    public Task AddEntityToRoom( int room_id, RoomEntityDto entity ) => this.cache.AddEntityToRoom( room_id, entity );
+
+    public Task RemoveEntityFromRoom( int room_id, int instance_id ) => this.cache.RemoveEntityFromRoom( room_id, instance_id );
 }
