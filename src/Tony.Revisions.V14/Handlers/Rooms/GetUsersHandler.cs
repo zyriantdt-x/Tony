8d8f3d6a02004c58605a1367feae2922edba8a33
Ellis <me@ellis.pw>
2025-03-09 16:28:27 +0000

    fix gusrs

--- src/Tony.Revisions.V14/Handlers/Rooms/GetUsersHandler.cs
+++ src/Tony.Revisions.V14/Handlers/Rooms/GetUsersHandler.cs
@@ -6,21 +6,27 @@ using Tony.Sdk.Services;
 namespace Tony.Revisions.V14.Handlers.Rooms;
 [Header( 61 )]
 class GetUsersHandler : IHandler {
-    //private readonly RoomEntityService entity_service;
+    private readonly IRoomEntityService entity_service;
     private readonly IRoomDataService room_data;
+    private readonly IPlayerService player_data;
 
-    public GetUsersHandler( /*RoomEntityService entity_service,*/ IRoomDataService room_data ) {
-        //this.entity_service = entity_service;
+    public GetUsersHandler( IRoomEntityService entity_service, IRoomDataService room_data, IPlayerService player_data ) {
+        this.entity_service = entity_service;
         this.room_data = room_data;
+        this.player_data = player_data;
     }
 
     public async Task Handle( ITonyClient client, object ClientMessage ) {
-        /*RoomDataDto? room = await this.room_data.GetPlayerRoomData( client.PlayerId ?? 0 );
+        PlayerRoomDto? player_room = await this.player_data.GetPlayerRoom( client.PlayerId );
+        if( player_room is null )
+            return;
+
+        RoomDataDto? room = await this.room_data.GetRoomDataById( player_room.RoomId );
         if( room is null )
             return;
 
         await client.SendAsync( new UserObjectsComposer() {
             Entities = await this.entity_service.GetEntitiesInRoom( room.Id )
-        } );*/
+        } );
     }
 }
